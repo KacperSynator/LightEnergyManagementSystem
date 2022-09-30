54e0ea9457be8c524bd0586532a50e4739d7a06d
KacperSynator <252844@student.pwr.edu.pl>
2022-09-30 21:56:08 +0200

    LocalRpi: add rust-protobuf library with demo

--- LocalRpi/src/main.rs
+++ LocalRpi/src/main.rs
@@ -3,6 +3,11 @@ use log::info;
 mod ble_connection;
 
 use ble_connection::BLEConnection;
+use protobuf::{EnumOrUnknown, Message};
+
+include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+use lamp_controller::LampData;
 
 
 #[tokio::main]
@@ -10,8 +15,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
     let ble_conn = BLEConnection::new().await?;
+
     ble_conn.write_to_device(&String::from("EC:62:60:93:A4:B2"), &String::from("hello from LocalRPi")).await?;
-    info!("Read data: {:?}", ble_conn.read_devices_data().await?);
+    let lamps_data = ble_conn.read_devices_data().await?;
+    info!("Received data: {:?}", lamps_data);
+    for lamp_data in lamps_data.iter() {
+        info!("Protobuf data: {:?}", LampData::parse_from_bytes(lamp_data).unwrap());
+    }
     
     Ok(())
 }
