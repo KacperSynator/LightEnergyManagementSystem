839d1719b3c5ffd06f1890e7232a3ec371b5c581
KacperSynator <kacper.synator@gmail.com>
2022-10-17 17:34:09 +0200

    ServerRpi: move .proto file to repo_root/proto

--- ServerRpi/build.rs
+++ ServerRpi/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     protobuf_codegen::Codegen::new()
         .cargo_out_dir("protos")
-        .include("src")
-        .input("src/light_energy_menagment_system.proto")
+        .include("../proto")
+        .input("../proto/light_energy_menagment_system.proto")
         .run_from_script();
 }
