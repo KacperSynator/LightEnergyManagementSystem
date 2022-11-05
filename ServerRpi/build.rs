4ec385c74edbb9aee8e3b5bb950745a5f46f8c9e
KacperSynator <kacper.synator@gmail.com>
2022-11-05 22:07:00 +0100

    ServerRpi: fix path in build.rs

--- ServerRpi/build.rs
+++ ServerRpi/build.rs
@@ -2,6 +2,6 @@ fn main() {
     protobuf_codegen::Codegen::new()
         .cargo_out_dir("protos")
         .include("../proto")
-        .input("../proto/light_energy_menagment_system.proto")
+        .input("../proto/light_energy_management_system.proto")
         .run_from_script();
 }
