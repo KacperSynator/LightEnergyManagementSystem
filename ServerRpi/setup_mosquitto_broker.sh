13a14ac720e73e926b8a2072b8ede2426d436ed8
KacperSynator <kacper.synator@gmail.com>
2022-11-13 16:31:10 +0100

    ServerRpi: update setup_broker script

--- ServerRpi/setup_mosquitto_broker.sh
+++ ServerRpi/setup_mosquitto_broker.sh
@@ -5,5 +5,7 @@ CONFIG_FILENAME=server_rpi.conf
 
 sudo echo "listener 1883" > $CONFIG_DIR_PATH/$CONFIG_FILENAME
 sudo echo "allow_anonymous true" >> $CONFIG_DIR_PATH/$CONFIG_FILENAME
+sudo kill $(ps aux |awk '/mosquitto/ {print $2}')
 sudo systemctl stop mosquitto.service
+sudo service mosquitto stop
 sudo mosquitto -c $CONFIG_DIR_PATH/$CONFIG_FILENAME -d
